a851b18708fed87f690c45e4541f44835dfc8b5d
Adam <adam.mcgladdery@hotmail.com>
2024-04-10 11:56:38 +0100

    Added entry pages and links on index page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
+from markdown import Markdown
 
 from . import util
 
+mk = Markdown()
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    if title in util.list_entries():
+        return render(request, "encyclopedia/entry.html", {
+            "text": mk.convert(util.get_entry(title)),
+            "title": title
+        })
+    else:
+        return render(request, "encyclopedia/error.html", {
+            "message": "Page does not exist",
+            "title": title
+        })
