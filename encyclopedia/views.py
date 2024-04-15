adba582f9c1b146b1f1ae8c1a21e1496f6c268dc
Adam <adam.mcgladdery@hotmail.com>
2024-04-15 10:49:21 +0100

    Added search function

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from markdown import Markdown
 
 from . import util
@@ -21,3 +21,36 @@ def entry(request, title):
             "message": "Page does not exist",
             "title": title
         })
+    
+def search(request):
+    entries = util.list_entries()
+    q = request.GET.get("q", "")
+    if q in entries:
+        return redirect("entry", title=q)
+    else:
+        list = []
+        for entry in entries:
+            if q.lower() in entry.lower():
+                list.append(entry)
+        return render(request, "encyclopedia/search.html", {
+            "entries": list,
+            "title": q
+        })
+
+
+
+    # if q in util.list_entries():
+    #     return render(request, "encyclopedia/entry.html", {
+    #         "text": mk.convert(util.get_entry(q)),
+    #         "title": q
+    #     })
+    # else:
+    #     list = []
+    #     for i in util.list_entries():
+    #         if q in i:
+    #             list.append(i)
+    #     return render(request, "encyclopedia/search.html", {
+    #         "entres": list,
+    #         "text": mk.convert(util.get_entry(q)),
+    #         "title": q
+    #     })
